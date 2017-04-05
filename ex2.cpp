1996f35e0ddfb9e0f22503b38ada982ef462cbdd
student-repo <repostudent@gmail.com>
2017-04-05 12:33:00 +0200

    task 1 and 2 v1.0

--- ex2.cpp
+++ ex2.cpp
@@ -2,12 +2,15 @@
 #include<stdlib.h>
 #include<limits.h>
 #include <time.h>
+#include<algorithm>
+
+using namespace std;
+
+#define RED   "\x1B[31m"
+#define RESET "\x1B[0m"
 
 int *insertionSort(int *a, int n); //a - array, n - length
-void merge(int *a, int p, int q, int r); //a - array p -first index, q - array length middle r - last array index
-void mergeSort(int *a, int p, int r); //a - array p -first index, r - last array index
 int partition(int *a, int p, int r);
-void quicksort2(int *a, int p, int r);//a - array p -first index, r - last array index
 void swap(int *A, int i, int j);
 int *getRandomArray(int n, int range);
 int *getSortedArray(int n, int range);
@@ -16,8 +19,6 @@ void displayArray(int *a, int n);
 int *copyArray(int *a, int n);
 void quicksort(int *a, int p, int r);
 void compareArrays(int *a, int *aa, int n);
-void merge2(int *A,int *L,int leftCount,int *R,int rightCount);
-void mergeSort2(int *A,int n);
 void resetCounrers();
 double getInsertionSortTime(int *a, int n);
 double getMergesortTime(int *a, int n);
@@ -28,21 +29,46 @@ int partition(int arr[], int l, int r, int x);
 int kthSmallest(int arr[], int l, int r, int k);
 int findMedian(int arr[], int n);
 int *generateRandomPermutation(int n);
+double getRandSelectTime(int *a, int n, int k);
+double getSelectTime(int *a, int n, int k);
+int random_selectionDemonstration(int* arr, int p, int r, int k);
+void printArray(int * array, int size);
+int randomPartitionDemonstration(int *a, int p, int r);
+int findMedianDemonstration(int arr[], int n);
+int kthSmallestDemonstration(int arr[], int l, int r, int k);
+
+
+
+int randomSelectionIndex;
+int selectionIndex;
+
 
 typedef struct Counter{
   int comparisonNumber;
   int swapNumber;
 }Counter;
 
-Counter insertionSortInfo = {0, 0}, mergeSortInfo = {0, 0}, quicksortInfo = {0, 0};
+Counter randomSelectionInfo = {0, 0}, selectionInfo = {0, 0};
 
 int main(){
-  int *a, *insertionSortArray, *mergeSortArray, *quicksortArray, range, arraySize, foo, i, *aa, selectionNumber;
-  char arrayType;
+  int *a, *insertionSortArray, *mergeSortArray, *quicksortArray, range, arraySize, foo, i, *aa, *aaa, selectionNumber, interval = 1;
+  char arrayType, programMode;
   double seconds;
   FILE *fp;
+  srand(time(NULL));
   clock_t start = clock();
   clock_t end = clock();
+  printf("1. Single selection test\n");
+  printf("2. Statistics\n" );
+  printf("3. Work demonstration\n" );
+  printf("chose: ");
+  scanf("%c", &programMode);
+  while(programMode != '1' && programMode != '2'&& programMode != '3'){
+    printf("Incorrect intput! \n");
+    printf("choose: ");
+    scanf("%c", &programMode);
+  }
+  printf("---------------------------------------------\n" );
   printf("1. Generate random array\n");
   printf("2. Generate permutation array\n");
   printf("chose: ");
@@ -61,9 +87,6 @@ int main(){
   printf("Data size: ");
   scanf("%d",&arraySize);
 
-  printf("selection number: ");
-  scanf("%d",&selectionNumber);
-
 if(arrayType == '1'){
   a = getRandomArray(arraySize, range);
 }
@@ -71,34 +94,129 @@ else{
   a = generateRandomPermutation(arraySize);
 }
 
-foo = arraySize / 100;
-Counter *insertionSortCounter, *mergeSortCounter, *quicksortCounter;
-insertionSortCounter = new Counter[foo];
-mergeSortCounter = new Counter[foo];
-quicksortCounter = new Counter[foo];
+
+if(programMode == '1'){
+  printf("selection number: ");
+  scanf("%d",&selectionNumber);
+  aaa =copyArray(a, arraySize);
+
+
+  printf("\n\ntest\n" );
+  printf("-----------------------------------------------------------\n" );
+  printf("[");
+    for(int ii = 0; ii < arraySize; ii++){
+      printf("%d, ",a[ii] );
+    }
+    printf("]\n");
+
+
+
+  int x = random_selection(a, 0, arraySize -1, selectionNumber);
+
+printf("\n\nRandom Selection\n" );
+printf("-----------------------------------------------------------\n" );
+printf("[");
+  for(int ii = 0; ii < arraySize; ii++){
+    if(ii == randomSelectionIndex){
+      printf(RED "%d," RESET, a[ii]);
+    }
+    else{
+      printf("%d, ",a[ii] );
+    }
+  }
+  printf("]\n");
+
+  quicksort(a, 0, arraySize -1);
+
+  printf("[" );
+    for(int ii = 0; ii < arraySize; ii++){
+      if(ii == selectionNumber - 1){
+        printf(RED "%d," RESET, a[ii]);
+      }
+      else{
+        printf("%d, ",a[ii] );
+      }
+    }
+    printf("]\n");
+
+
+x = kthSmallest(aaa, 0, arraySize -1, selectionNumber);
+    printf("\n\n Selection\n" );
+    printf("-----------------------------------------------------------\n" );
+    printf("[");
+      for(int ii = 0; ii < arraySize; ii++){
+        if(ii == selectionIndex){
+          printf(RED "%d," RESET, aaa[ii]);
+        }
+        else{
+          printf("%d, ",aaa[ii] );
+        }
+      }
+      printf("]\n");
+
+      quicksort(aaa, 0, arraySize -1);
+
+      printf("[" );
+        for(int ii = 0; ii < arraySize; ii++){
+          if(ii == selectionNumber - 1){
+            printf(RED "%d," RESET, aaa[ii]);
+          }
+          else{
+            printf("%d, ",aaa[ii] );
+          }
+        }
+        printf("]\n");
+
+
+  exit(1);
+}
+
+if(programMode == '3'){
+  printf("selection number: ");
+  scanf("%d",&selectionNumber);
+  aaa =copyArray(a, arraySize);
+
+  printf("\n\nRandom Selection\n" );
+  printf("-----------------------------------------------------------\n" );
+  random_selectionDemonstration(a, 0, arraySize -1, selectionNumber);
+
+
+
+  printf("\n\n Selection\n" );
+  printf("-----------------------------------------------------------\n" );
+
+kthSmallestDemonstration(aaa, 0, arraySize -1, selectionNumber);
+  exit(1);
+}
+
+
+
+int k;
+
+foo = arraySize / interval;
+Counter *randomSelectionCounter, *selectionCounter;
+randomSelectionCounter = new Counter[foo];
+selectionCounter = new Counter[foo];
 
 fp = fopen("test.csv", "w+");
-fprintf(fp, "Data size,Insertionsort time time,Mergesort time,Quicksort time,     ,\n");
+fprintf(fp, "Data size,random selection time,selection time,     ,\n");
 
 for(i = 1; i <= foo; i++){
   if(arrayType == '1'){
     a = getRandomArray(i * 100, range);
   }
   else{
-    a = getSortedArray(i * 100, range);
+    a = generateRandomPermutation(i * 100);
   }
 
-  fprintf(fp, "%d,",i * 100);
-  fprintf(fp, "%.10e,",getInsertionSortTime(a, i * 100));
-  fprintf(fp, "%.10e,", getMergesortTime(a, i * 100));
-  fprintf(fp, "%.10e\n",getQuicksortTime(a, i * 100));
+  fprintf(fp, "%d,",i * interval);
+  srand(time(NULL));
+  k = rand() % (i * interval);
+  fprintf(fp, "%.10e,",getRandSelectTime(a, i * interval, k));
+  fprintf(fp, "%.10e\n",getSelectTime(a, i * interval, k));
   free(a);
-  insertionSortCounter[i - 1].comparisonNumber = insertionSortInfo.comparisonNumber;
-  insertionSortCounter[i - 1].swapNumber = insertionSortInfo.swapNumber;
-  mergeSortCounter[i - 1].comparisonNumber = mergeSortInfo.comparisonNumber;
-  mergeSortCounter[i - 1].swapNumber = mergeSortInfo.swapNumber;
-  quicksortCounter[i - 1].comparisonNumber = quicksortInfo.comparisonNumber;
-  quicksortCounter[i - 1].swapNumber = quicksortInfo.swapNumber;
+  randomSelectionCounter[i - 1].comparisonNumber = randomSelectionInfo.comparisonNumber;
+  selectionCounter[i - 1].comparisonNumber = selectionInfo.comparisonNumber;
   resetCounrers();
 }
 
@@ -106,17 +224,21 @@ for(i = 1; i <= foo; i++){
 printf("\n\n" );
 
 // fp = fopen("test.csv", "w+");
-fprintf(fp, "Data size,Insertionsort comparison,Mergesort comparison,Quicksort comparison,     ,Data size,Insertionsort swap,Mergesort swap,Quicksort swap\n");
+fprintf(fp, "Data size,Random selection comparison,Selection comparison\n");
 for(i = 0; i < foo; i++){
-  fprintf(fp, "%d,%d,%d,%d,    ,%d,%d,%d,%d\n", (i + 1) * 100, insertionSortCounter[i].comparisonNumber, mergeSortCounter[i].comparisonNumber, quicksortCounter[i].comparisonNumber, (i + 1) * 100, insertionSortCounter[i].swapNumber, mergeSortCounter[i].swapNumber, quicksortCounter[i].swapNumber);
+  fprintf(fp, "%d,%d,%d\n", (i + 1) * interval, randomSelectionCounter[i].comparisonNumber, selectionCounter[i].comparisonNumber);
 }
  fclose(fp);
 
  system("libreoffice --calc test.csv &");
- free(insertionSortCounter);
- free(mergeSortCounter);
- free(quicksortCounter);
+free(randomSelectionCounter);
+free(selectionCounter);
 
+
+
+// int arr[] = {12, 3, 5, 7, 4, 19, 26};
+//
+// printf("%d\n",random_selectionDemonstration(arr, 0, 6, 2) );
 
 
 // int *a;
@@ -144,6 +266,7 @@ int randomPartition(int *a, int p, int r){
   swap(a, pivotIndex, p);
   int x = a[p], i = p, j, buffor;
   for(j = p + 1; j <= r; j++){
+    randomSelectionInfo.comparisonNumber++;
     if(a[j] <= x){
       i = i + 1;
       swap(a, i, j);
@@ -155,18 +278,27 @@ int randomPartition(int *a, int p, int r){
 
 int random_selection(int* arr, int p, int r, int k)
 {
-    if(p == r)
+    if(p == r){
+      randomSelectionIndex = p;
         return arr[p];
+    }
 
-    if(k ==0) return -1;
+
+    if(k ==0){
+      randomSelectionIndex = -1;
+      return -1;
+    }
 
     if(p < r)
     {
 
     int mid = randomPartition(arr, p, r);
     int i = mid - p + 1;
-    if(i == k)
-        return arr[mid];
+    if(i == k){
+    //   printf("random selection  index = %d\n", mid );
+    randomSelectionIndex = mid;
+    return arr[mid];
+    }
     else if(k < i)
         return random_selection(arr, p, mid-1, k);
     else
@@ -204,8 +336,10 @@ int kthSmallest(int arr[], int l, int r, int k)
         int pos = partition(arr, l, r, medOfMed);
 
         // If position is same as k
-        if (pos-l == k-1)
-            return arr[pos];
+        if (pos-l == k-1){
+          selectionIndex = pos;
+          return arr[pos];
+        }
         if (pos-l > k-1)  // If position is more, recur for left
             return kthSmallest(arr, l, pos-1, k);
 
@@ -214,6 +348,7 @@ int kthSmallest(int arr[], int l, int r, int k)
     }
 
     // If k is more than number of elements in array
+    selectionIndex = INT_MAX;
     return INT_MAX;
 }
 
@@ -236,6 +371,7 @@ int partition(int arr[], int l, int r, int x)
     i = l;
     int  j, buffor;
     for(j = l + 1; j <= r; j++){
+      selectionInfo.comparisonNumber++;
       if(arr[j] <= x){
         i = i + 1;
         swap(arr, i, j);
@@ -255,15 +391,13 @@ int *insertionSort(int *a, int n){
   for(j = 1; j < n; j++){
     key = a[j];
     i = j - 1;
-    insertionSortInfo.comparisonNumber++;
+    selectionInfo.comparisonNumber++;
     while((i >= 0) && (a[i] > key)){
       a[i + 1] = a[i];
       i--;
-      insertionSortInfo.comparisonNumber++;
-      insertionSortInfo.swapNumber++;
+      randomSelectionInfo.comparisonNumber++;
     }
     a[i + 1] = key;
-    insertionSortInfo.swapNumber++;
   }
     return a;
 }
@@ -277,165 +411,24 @@ void swap(int *A, int i, int j) {
 void quicksort(int *a, int p, int r){
     int i = p, j = r, x, q = a[(p + r) / 2];
     do{
-      quicksortInfo.comparisonNumber++;
     while (a[i] < q){
-      // printf("Move left array index one position to the right\n");
       i++;
-      quicksortInfo.comparisonNumber++;
     }
-    quicksortInfo.comparisonNumber++;
     while (a[j] > q){
-      // printf("Move right array index one position to the left\n");
-      quicksortInfo.comparisonNumber++;
       j--;
     }
     if (i <= j){
-      // printf("Swap a[%d] = %d and a[%d] = %d\n",i, a[i], j, a[j] );
-      quicksortInfo.swapNumber += 2;
       swap(a, i, j);
       i++; j--;
     }
     }while (i<=j);
     if (j > p){
-      // printf("Call quicksort(array, %d, %d)\n", p, j);
       quicksort(a, p, j);
     }
     if (i < r){
-      // printf("Call quicksort(array, %d, %d)\n", i, r);
       quicksort(a, i, r);
     }
   }
-
-  void merge(int *a, int p, int q, int r){
-    int n1 = q - p + 1, n2 = r - q, *ll, *rr, i, j, k;
-    ll = new int[n1];
-    rr = new int[n2];
-
-    // printf("Create first sub-array\n" );
-    for(i = 0; i < n1; i++){
-      ll[i] = a[p + i];
-    }
-    // printf("Create second sub-array\n" );
-    for(i = 0; i < n2; i++){
-        rr[i] = a[q + i + 1];
-    }
-    i = 0; j = 0;k = p;
-    // printf("Merge 2 sorted arrays\n" );
-    while(i < n1 && j < n2) {
-      if(ll[i]  < rr[j]) a[k++] = ll[i++];
-      else a[k++] = rr[j++];
-      mergeSortInfo.comparisonNumber++;
-      mergeSortInfo.swapNumber += 2;
-    }
-    while(i < n1){
-      a[k++] = ll[i++];
-      mergeSortInfo.swapNumber += 2;
-    }
-    while(j < n2){
-      a[k++] = rr[j++];
-      mergeSortInfo.swapNumber += 2;
-    }
-    free(ll);
-    free(rr);
-  }
-
-    void mergeSort(int *a, int p, int r){
-      int q;
-      if(p < r){
-      q = (int)((p + r) / 2);
-      // printf("Call mergeSort(array, %d, %d)\n",p, q );
-      mergeSort(a, p, q);
-      // printf("Call mergeSort(array, %d, %d)\n",q + 1, r );
-      mergeSort(a, q + 1, r);
-      // printf("Call merge(array, %d, %d, %d)\n",p, q, r );
-      merge(a, p, q, r);
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-  void quicksort2(int *a, int p, int r){
-    int q;
-    if(p < r){
-      q = partition(a, p, r);
-      quicksort2(a, p, q - 1);
-      quicksort2(a, q + 1, r);
-    }
-  }
-
-  int partition(int *a, int p, int r){
-    int x = a[r], i = p - 1, j, buffor;
-    for(j = p; j <= r - 1; j++){
-      quicksortInfo.comparisonNumber++;
-      if(a[j] <= x){
-        i = i + 1;
-        swap(a, i, j);
-        quicksortInfo.swapNumber += 2;
-      }
-    }
-    swap(a, i + 1, r);
-    quicksortInfo.swapNumber += 2;
-    return i + 1;
-  }
-
-  void merge2(int *a,int *p,int np,int *r,int rc) {
-  	int i,j,k;
-  	i = 0; j = 0; k =0;
-
-  	while(i < np && j< rc) {
-  		if(p[i]  < r[j]) a[k++] = p[i++];
-  		else a[k++] = r[j++];
-      mergeSortInfo.comparisonNumber++;
-      mergeSortInfo.swapNumber += 2;
-  	}
-  	while(i < np){
-      a[k++] = p[i++];
-      mergeSortInfo.swapNumber += 2;
-    }
-  	while(j < rc){}
-    a[k++] = r[j++];
-    mergeSortInfo.swapNumber += 2;
-  }
-
-  void mergeSort2(int *a,int n) {
-  	int q,i, *p, *r;
-  	if(n < 2) return;
-  	q = n/2;
-  	p = new int[q];
-  	r = new int[n - q];
-
-  	for(i = 0;i < q; i++) p[i] = a[i];
-  	for(i = q;i < n; i++) r[i - q] = a[i];
-
-  	mergeSort2(p, q);
-  	mergeSort2(r,n - q);
-  	merge2(a, p, q, r, n - q);
-          free(p);
-          free(r);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -475,8 +468,6 @@ void quicksort(int *a, int p, int r){
     a = getRandomArray(n, range);
     quicksort(a, 0, n - 1);
     reverseArray(a, n);
-    quicksortInfo.comparisonNumber = 0;
-    quicksortInfo.swapNumber = 0;
     return a;
   }
 
@@ -511,51 +502,32 @@ void quicksort(int *a, int p, int r){
   }
 
 void resetCounrers(){
-  insertionSortInfo.comparisonNumber = 0;
-  insertionSortInfo.swapNumber = 0;
-  mergeSortInfo.comparisonNumber = 0;
-  mergeSortInfo.swapNumber = 0;
-  quicksortInfo.comparisonNumber = 0;
-  quicksortInfo.swapNumber = 0;
+  selectionInfo.comparisonNumber = 0;
+  randomSelectionInfo.comparisonNumber = 0;
 }
 
-double getInsertionSortTime(int *a, int n){
+double getSelectTime(int *a, int n, int k){
   clock_t start = clock();
   clock_t end = clock();
   int *aa;
   double seconds;
   aa = copyArray(a, n);
   start = clock();
-  insertionSort(aa, n);
+  kthSmallest(aa, 0, n - 1, k);
   end = clock();
   seconds = (double)(end - start) / CLOCKS_PER_SEC;
   free(aa);
   return seconds;
 }
 
-double getMergesortTime(int *a, int n){
+double getRandSelectTime(int *a, int n, int k){
   clock_t start = clock();
   clock_t end = clock();
   int *aa;
   double seconds;
   aa = copyArray(a, n);
   start = clock();
-  // mergeSort(aa, n);
-   mergeSort(aa, 0, n - 1);
-  end = clock();
-  seconds = (double)(end - start) / CLOCKS_PER_SEC;
-  free(aa);
-  return seconds;
-}
-
-double getQuicksortTime(int *a, int n){
-  clock_t start = clock();
-  clock_t end = clock();
-  int *aa;
-  double seconds;
-  aa = copyArray(a, n);
-  start = clock();
-  quicksort(aa, 0, n - 1);
+  random_selection(aa, 0, n - 1, k);
   end = clock();
   seconds = (double)(end - start) / CLOCKS_PER_SEC;
   free(aa);
@@ -572,8 +544,132 @@ int *generateRandomPermutation(int n){
   srand(time(NULL));
   for(int j = 0; j < n; j++){
     index = rand() % n;
-    printf("index = %d   i = %d\n",index, j );
     swap(a, index, j);
   }
     return a;
+}
+
+
+
+
+
+
+
+
+
+
+int random_selectionDemonstration(int* arr, int p, int r, int k)
+{
+  printArray(arr + p, r - p + 1);
+    if(p == r){
+      printf("p == r    return value: %d\n", arr[p]);
+        return arr[p];
+    }
+
+
+    if(k ==0){
+      return -1;
+    }
+
+    if(p < r)
+    {
+
+    int mid = randomPartitionDemonstration(arr, p, r);
+    int i = mid - p + 1;
+    if(i == k){
+      printf("%d smaller elements before pivot    return value: %d\n",k - 1,  arr[mid]);
+    return arr[mid];
+    }
+    else if(k < i)
+        return random_selectionDemonstration(arr, p, mid-1, k);
+    else
+        return random_selectionDemonstration(arr, mid+1, r, k-i);
+    }
+
+}
+
+int randomPartitionDemonstration(int *a, int p, int r){
+  srand(time(NULL));
+  int pivotIndex = p + rand()% (r-p);
+  printf("pivot = %d\n",a[pivotIndex] );
+  swap(a, pivotIndex, p);
+  int x = a[p], i = p, j, buffor;
+  for(j = p + 1; j <= r; j++){
+    if(a[j] <= x){
+      i = i + 1;
+      swap(a, i, j);
+    }
+  }
+  swap(a, i, p);
+  return i;
+}
+
+void printArray(int * array, int size){
+
+  int i;
+  printf("[ ");
+  for (i = 0; i < size; i++)
+    printf("%d ", array[i]);
+  printf("]\n");
+}
+
+
+int kthSmallestDemonstration(int arr[], int l, int r, int k)
+{
+    // If k is smaller than number of elements in array
+    if (k > 0 && k <= r - l + 1)
+    {
+      printArray(arr + l, r - l + 1);
+        int n = r-l+1; // Number of elements in arr[l..r]
+
+        // Divide arr[] in groups of size 5, calculate median
+        // of every group and store it in median[] array.
+        int i, median[(n+4)/5]; // There will be floor((n+4)/5) groups;
+        for (i=0; i<n/5; i++)
+            median[i] = findMedian(arr+l+i*5, 5);
+        if (i*5 < n) //For last group with less than 5 elements
+        {
+            median[i] = findMedian(arr+l+i*5, n%5);
+            i++;
+        }
+        printf("determine medians:\n");
+        printArray(median, (n+4)/5);
+
+        // Find median of all medians using recursive call.
+        // If median[] has only one element, then no need
+        // of recursive call
+        int medOfMed = (i == 1)? median[i-1]:
+                                 kthSmallestDemonstration(median, 0, i-1, i/2);
+
+        printf("determine median of medians = %d\n", medOfMed );
+
+        // Partition the array around a random element and
+        // get position of pivot element in sorted array
+        printf("pivot = %d\n",arr[medOfMed] );
+        int pos = partition(arr, l, r, medOfMed);
+
+
+        // If position is same as k
+        if (pos-l == k-1){
+          printf("return value = %d \n", arr[pos]);
+          selectionIndex = pos;
+          return arr[pos];
+        }
+        if (pos-l > k-1)  // If position is more, recur for left
+            return kthSmallestDemonstration(arr, l, pos-1, k);
+
+        // Else recur for right subarray
+        return kthSmallestDemonstration(arr, pos+1, r, k-pos+l-1);
+    }
+
+    // If k is more than number of elements in array
+    selectionIndex = INT_MAX;
+    return INT_MAX;
+}
+
+int findMedianDemonstration(int arr[], int n)
+{
+    // sort(arr, arr+n);  // Sort the array
+    insertionSort(arr, n);
+    return arr[n/2];   // Return middle element
 }
